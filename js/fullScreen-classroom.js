c5aa3ab4d03cbef4e4f0f3094cd833db797cbe8c
ivy2846357 <ivy2846357@icloud.com>
2022-09-19 15:19:08 +0800

    修改圖片路徑位置

--- js/fullScreen-classroom.js
+++ js/fullScreen-classroom.js
@@ -3,17 +3,17 @@ const classroomTalkBlockImg = document.querySelector('.classroom-talk-block')
 
 function checkImgState(){
     if(window.innerWidth <= 768){
-        classroomTalkBlockImg.src = '../img/classroom-talk-block-sm.png'
+        classroomTalkBlockImg.src = './img/classroom-talk-block-sm.png'
     }else{
-        classroomTalkBlockImg.src = '../img/classroom-talk-block-lg.png'
+        classroomTalkBlockImg.src = './img/classroom-talk-block-lg.png'
     }
 }
 
 window.addEventListener('resize', function(){
     if(window.innerWidth < 768){
-        classroomTalkBlockImg.src = '../img/classroom-talk-block-sm.png'
+        classroomTalkBlockImg.src = './img/classroom-talk-block-sm.png'
     }else{
-        classroomTalkBlockImg.src = '../img/classroom-talk-block-lg.png'
+        classroomTalkBlockImg.src = './img/classroom-talk-block-lg.png'
     }
 })
 
--- js/fullScreen-video.js
+++ js/fullScreen-video.js
@@ -3,21 +3,21 @@ const videoBackgroundTopImg = document.querySelector('.video-background-top')
 
 function checkImgState(){
     if(window.innerWidth > 1200){
-        videoBackgroundTopImg.src = '../img/video-bg-top-pc.png'
+        videoBackgroundTopImg.src = './img/video-bg-top-pc.png'
     }else if(window.innerWidth < 992){
-        videoBackgroundTopImg.src = '../img/video-bg-top-pad.png'
+        videoBackgroundTopImg.src = './img/video-bg-top-pad.png'
     }else if(window.innerWidth < 576){
-        videoBackgroundTopImg.src = '../img/video-bg-top-phone.png'
+        videoBackgroundTopImg.src = './img/video-bg-top-phone.png'
     }
 }
 
 window.addEventListener('resize', function(){
     if(window.innerWidth > 1200){
-        videoBackgroundTopImg.src = '../img/video-bg-top-pc.png'
+        videoBackgroundTopImg.src = './img/video-bg-top-pc.png'
     }else if(window.innerWidth < 992){
-        videoBackgroundTopImg.src = '../img/video-bg-top-pad.png'
+        videoBackgroundTopImg.src = './img/video-bg-top-pad.png'
     }else if(window.innerWidth < 576){
-        videoBackgroundTopImg.src = '../img/video-bg-top-phone.png'
+        videoBackgroundTopImg.src = './img/video-bg-top-phone.png'
     }
 })
 
