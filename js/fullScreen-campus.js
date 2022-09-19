1a8305a7355505b536abff652b22c801c0139f71
ivy2846357 <ivy2846357@icloud.com>
2022-09-19 17:22:18 +0800

    微調校園版+教室版介面手機版圖片位置

--- js/fullScreen-campus.js
+++ js/fullScreen-campus.js
@@ -4,9 +4,9 @@ const campusRightTree = document.querySelector('.campus-tree-right');
 // let leftTreePosition = -200;
 // let rightTreePosition = -370;
 
-window.addEventListener('resize', function(){
-    leftTreePosition -= 10;
-    rightTreePosition -= 10;
-    campusLeftTree.style.left = leftTreePosition + 'px';
-    campusRightTree.style.right = rightTreePosition + 'px';
-})
+// window.addEventListener('resize', function(){
+//     leftTreePosition -= 10;
+//     rightTreePosition -= 10;
+//     campusLeftTree.style.left = leftTreePosition + 'px';
+//     campusRightTree.style.right = rightTreePosition + 'px';
+// })
